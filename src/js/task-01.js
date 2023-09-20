392c2a23932a52cfb640243b84135e333fb42183
irokez34 <jhmdaurel@gmail.com>
2023-09-20 22:44:46 +0300

    task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+//TASK 1
+const listCategories = document.querySelectorAll('.item');
+console.log('Number of categories: ', listCategories.length);
+
+// TASK 2
+
+const itemHead = document.querySelectorAll("h2");
+itemHead.forEach((head) => console.log(head))
+
+
+console.log(itemHead);
+
