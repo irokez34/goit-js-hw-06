c770261b3efd6509cf0eddbe7b880e79de1878a7
irokez34 <jhmdaurel@gmail.com>
2023-10-17 19:27:05 +0300

    hw_8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
 
+const form = document.querySelector('.login-form');
+
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const form = evt.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === "" || password === "") {
+    return alert("форма не заповнена");
+  }
+  const data = {
+                email: `${email}`,
+                password: `${password}`
+              };
+  console.log(data);
+  form.reset();
+}
