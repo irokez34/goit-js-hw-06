da904a3bbe425b2313fa45dba7d7a4de0a99d272
irokez34 <jhmdaurel@gmail.com>
2023-10-17 20:30:44 +0300

    hw_10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxesCon = document.querySelector('#boxes');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+function createBoxes(amount) {
+amount = input.value;
+const boxes = [];
+let size = 30;
+
+for (let i = 0; i < amount; i++) {
+ const box = document.createElement('div');
+ box.style.width = `${size}px`;
+ box.style.height =`${size}px`;
+ box.style.backgroundColor = getRandomHexColor();
+ boxes.push(box);
+ size +=10;
+}
+boxesCon.append(...boxes);
+}
+function destroyBoxes (){
+  boxes.innerHTML ='';
+}
