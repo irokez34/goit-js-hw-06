fdc082b8170ce30a1caaa5e8c9cea92acc1aa66d
irokez34 <jhmdaurel@gmail.com>
2023-10-18 19:31:14 +0300

    task-1-space_deleted

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,16 +2,9 @@
 const categoriesList = document.querySelector("#categories");
 const listLength = categoriesList.children; 
 const listArray = [...listLength];
-
-
 console.log('Number of categories: ', listArray.length);
 
 // TASK 1.2
-
-
-
-
-
 
 listArray.forEach((item) =>  {
   const categorieHead = item.querySelector('h2')
