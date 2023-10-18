7fbde3d006b1185c71e1c186845649e5e5d7a5a9
irokez34 <jhmdaurel@gmail.com>
2023-10-18 19:30:18 +0300

    task-1-rework

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,19 +1,23 @@
-//TASK 1
-const listCategories = document.querySelectorAll('.item');
-console.log('Number of categories: ', listCategories.length);
+// TASK 1
+const categoriesList = document.querySelector("#categories");
+const listLength = categoriesList.children; 
+const listArray = [...listLength];
+
+
+console.log('Number of categories: ', listArray.length);
 
 // TASK 1.2
 
-const categoriesList = document.querySelector("#categories");
-const listItem = categoriesList.querySelectorAll(".item");
 
 
 
 
-listItem.forEach((item) =>  {
+
+listArray.forEach((item) =>  {
   const categorieHead = item.querySelector('h2')
   const categorieItemsCount = item.querySelectorAll('li').length;
   console.log(`Категорія: ${categorieHead.textContent}`);
   console.log(`Кількість елементів: ${categorieItemsCount}`);
+
 });
 
