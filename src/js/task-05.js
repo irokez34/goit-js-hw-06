66375c5e530b3844be35531fe5d3e2dd6b1e4992
irokez34 <jhmdaurel@gmail.com>
2023-10-18 19:32:46 +0300

    task-5-reworked

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -6,5 +6,5 @@ const output = document.querySelector("#name-output");
 
 input.addEventListener("input", event => 
 {
-  output.textContent = event.currentTarget.value||`Anonymous`;
+  output.textContent = event.currentTarget.value.trim()||`Anonymous`;
 })
